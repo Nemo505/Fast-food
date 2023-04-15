ecf3ecf5dac496615740abbbda8b8e8e82844de5
nemo <dahliahalesia8@gmail.com>
2023-04-15 23:51:59 +0630

    filterSolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 const Navbar = ({appDrinks, setAppDrinks}) => {
   const [search, setSearch] = useState('');
-
+  
   useEffect(() => {
-    hi()
-  }, []);
-
-  const hi = async() => {
-    const filter = appDrinks.filter(e => e.strDrink.toLowerCase().includes(search));
-    // const filter = appDrinks.filter(e =>{
-    //   return (e.toLowerCase().search(e.target.value.toLowerCase().includes(search)));
-    // });
+    const filter = appDrinks.filter(e => e.strDrink.toLowerCase().includes(search.toLowerCase()));
     console.log(filter);
     setAppDrinks(filter);
-  }
+  }, [search]);
+
+  
   return (
     <div className='bg-fuchsia-300 flex justify-between p-5'>
       <div>maple</div>
