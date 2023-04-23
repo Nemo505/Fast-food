7333f3bdf99b349cb75b9a810d27ecaba5e8661c
nemo <dahliahalesia8@gmail.com>
2023-04-23 13:57:27 +0630

    detail

--- src/components/Foods/Detail.jsx
+++ src/components/Foods/Detail.jsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Cart from '../Carts/Cart'
 
 const Detail = () => {
   let {id} = useParams();
   const [drinkDetail, setDrinkDetail] = useState({});
 
   useEffect(() => {
-    drinkDetailData();
+    detailData();
   }, []);
 
-const drinkDetailData = async() => {
-  await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-   .then((response) => response.json())
-   .then((data) => {
-      console.log(data);
-      setDrinkDetail(data.drinks[0]);
-   })
-   .catch((err) => {
-      console.log(err.message);
-   });
-}
+ const detailData = async() => {
+      await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=85641bf6e4024052a48dffb41b6dde93`)
+      .then((response) => response.json())
+      .then((data) => {
+          console.log(data);
+          setDrinkDetail(data)
+      })
+      .catch((err) => {
+          console.log(err.message);
+      });
+
+  }
+  
   return (
-    <div><img src={drinkDetail.strDrinkThumb} alt="" /></div>
+    <div><img src={drinkDetail.image} alt="" />
+      <Cart />
+    </div>
   )
+    
 }
 
 export default Detail
--- src/components/Foods/FoodCard.jsx
+++ src/components/Foods/FoodCard.jsx
@@ -6,25 +6,7 @@ const FoodCard = ({}) => {
   const {state:{drinks}} = StateContextCustom(state => state)
   const {dispatch} = StateContextCustom()
   const [limit, setLimit] = useState(8);
-  const {drinkDetail, setDrinkDetail} = StateContextCustom()
-
-  //price and detail api
-  useEffect(() => {
-      detailData();
-  }, []);
-
-  const detailData = async() => {
-      await fetch(`https://api.spoonacular.com/recipes/${drinkDetail}/information?apiKey=85641bf6e4024052a48dffb41b6dde93`)
-      .then((response) => response.json())
-      .then((data) => {
-          console.log(data);
-          setDrinkDetail(data)
-      })
-      .catch((err) => {
-          console.log(err.message);
-      });
-  }
-
+ 
   return (  
     <div className='bg-red-200 flex flex-wrap'>
       {
@@ -36,7 +18,7 @@ const FoodCard = ({}) => {
                 <Link to={`/detail/${drink.id}`}>Detail</Link> 
                 <h4> {drink.strDrink}</h4>
                 <button className='bg-red-600 p-2 rounded-md' 
-                  onClick={() => {dispatch({type:"ADD_TO_CART", payload:{drink, setDrinkDetail } }); setDrinkDetail(drink.id) }}  > 
+                  onClick={() => {dispatch({type:"ADD_TO_CART", payload:{drink} }) }}  > 
                   Add to cart
                 </button>
             </div>
